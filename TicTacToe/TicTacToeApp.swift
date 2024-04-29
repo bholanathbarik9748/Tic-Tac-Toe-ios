be66a31f046792f75f5ca436ad76fe32fd9356cf
Bholanath <bholanathbarik9748@gmail.com>
2024-04-29 19:07:41 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Bholanath Barik on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
